bf0e5bd9d94966abb41ae85e7da73c855aed9e8e
ppesterev <ppesterev@mail.ru>
2021-04-07 19:20:46 +0700

    cache static files with service worker

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
+window.addEventListener("load", () => {
+  navigator.serviceWorker
+    .register("./service-worker.js")
+    .then((reg) =>
+      console.log(`Service worker registered, scope: ${reg.scope}`)
+    );
+});
+
 ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_PREFIX = "bigtrip-cache";
+const CACHE_VER = "v1";
+const CACHE_NAME = `${CACHE_PREFIX}_${CACHE_VER}`;
+const CACHE_FILES = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/index.css",
+  "/css/style.css",
+  "/img/header-bg.png",
+  "/img/header-bg@2x.png",
+  "/img/logo.png"
+];
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CACHE_FILES))
+  );
+});
+
+self.addEventListener("activate", (evt) => {
+  console.log("Service worker activated");
+  evt.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.map((key) =>
+            key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME
+              ? caches.delete(key)
+              : Promise.resolve(null)
+          )
+        )
+      )
+  );
+});
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((cacheResponse) => {
+      if (cacheResponse) {
+        return cacheResponse;
+      }
+      return fetch(evt.request).then((response) => {
+        if (response && response.status === 200 && response.type === "basic") {
+          caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.put(evt.request, response.clone()));
+        }
+        return response;
+      });
+    })
+  );
+});

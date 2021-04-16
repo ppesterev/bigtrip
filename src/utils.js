83a71d03a3b48827b31c550596167851292c1e72
ppesterev <ppesterev@mail.ru>
2021-04-16 18:19:54 +0700

    remove unused util functions

--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,5 @@
 import { TypeCategory } from "./const";
 
-export function shuffleArray(originalArray) {
-  let arr = originalArray.slice();
-  for (let i = arr.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * i);
-    [arr[i], arr[j]] = [arr[j], arr[i]];
-  }
-  return arr;
-}
-
-export function randomFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
-
-export function randomNFromArray(arr, count) {
-  let shuffled = shuffleArray(arr);
-  return shuffled.slice(0, count);
-}
-
 export function capitalize(str) {
   return str.slice(0, 1).toUpperCase().concat(str.slice(1));
 }
